2ef90632ebfde15b6a300ae5b7947a9ebb1d41c4
marktan2017 <mark.tan.2017@sis.smu.edu.sg>
2020-11-15 03:22:05 +0800

    update

--- blackjack.py
+++ blackjack.py
@@ -35,6 +35,115 @@ class DeckHelpers:
                 totalSum += card[0]
         return totalSum
 
+class Player:
+    def __init__(self):
+        self.cards = []
+        pass
+
+    def drawCard(self):
+        self.cards.append(DeckHelpers.getCard())
+    
+    def drawBlackCard(self):
+        self.cards.append(DeckHelpers.getBlackCard())
+
+    def emptyCards(self):
+        self.cards = []
+    
+    def is21AboveOrBelow1(self):
+        playerSum = DeckHelpers.getSumOfCards(self.cards)
+        return (playerSum >= 21 or playerSum < 1)
+    
+    def promptAction(self):
+        playerInput = input("What would you like to do? Hit or Stick? (h/s)\n")
+        while playerInput != "h" and playerInput != "s":
+            playerInput = input("What would you like to do? Hit or Stick? (h/s)\n")
+        print("You have inputted: ", playerInput)
+        return playerInput
+    
+    def displayCards(self):
+        print("Your cards: ", self.cards)
+        print("Sum of cards: ", DeckHelpers.getSumOfCards(self.cards))
+
+class Dealer:
+    def __init__(self):
+        self.cards = []
+        pass
+
+    def drawCard(self):
+        self.cards.append(DeckHelpers.getCard())
+    
+    def drawBlackCard(self):
+        self.cards.append(DeckHelpers.getBlackCard())
+
+    def emptyCards(self):
+        self.cards = []
+    
+    def isAbove17orBust(self):
+        dealerSum = DeckHelpers.getSumOfCards(self.cards)
+        return dealerSum >= 17        
+
+class Qlearning(Player):
+    def __init__(self, epsilon=0.1, alpha=0.3, gamma=0.9):
+        super().__init__()
+        self.epsilon=epsilon
+        self.alpha=alpha
+        self.gamma=gamma
+        self.Q = {} #Q table
+        self.last_state=None
+        self.q_last=0.0
+        self.state_action_last=None
+
+    def resetLastStateVariables(self):
+        self.last_state = None
+        self.q_last = 0.0
+        self.state_action_last = None
+
+    def epslion_greedy(self, possible_moves): #esplion greedy algorithm
+        self.last_state = DeckHelpers.getSumOfCards(self.cards)
+        if(random.random() < self.epsilon):
+            move = random.choice(possible_moves)
+            self.state_action_last = (self.last_state, move)
+            self.q_last = self.getQ(self.last_state, move)
+            return move
+        else: #greedy strategy
+            Q_list=[]
+            for action in possible_moves:
+                Q_list.append(self.getQ(self.last_state, action))
+            maxQ=max(Q_list)
+
+            if Q_list.count(maxQ) > 1:
+                # more than 1 best option; choose among them randomly
+                best_options = [i for i in range(len(possible_moves)) if Q_list[i] == maxQ]
+                i = random.choice(best_options)
+            else:
+                i = Q_list.index(maxQ)
+            self.state_action_last = (self.last_state, possible_moves[i])
+            self.q_last = self.getQ(self.last_state, possible_moves[i])
+            return possible_moves[i]
+
+    def getQ(self, state, action): #get Q states
+        if(self.Q.get((state, action))) is None:
+            self.Q[(state, action)] = 0
+        return self.Q.get((state, action))
+
+    def updateQ(self, reward, possible_moves): # update Q states using Qleanning
+        q_list=[]
+        for moves in possible_moves:
+            q_list.append(self.getQ(DeckHelpers.getSumOfCards(self.cards), moves))
+        if q_list:
+            max_q_next = max(q_list)
+        else:
+            max_q_next = 0.0
+        self.Q[self.state_action_last] = self.q_last + self.alpha * ((reward + self.gamma * max_q_next) - self.q_last)
+
+    def saveQtable(self,file_name):  #save table
+        with open(file_name, 'wb') as handle:
+            pickle.dump(self.Q, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    def loadQtable(self,file_name): # load table
+        with open(file_name, 'rb') as handle:
+            self.Q = pickle.load(handle)
+
 class BackJackGame:
     def __init__(self, enableLog=True):
         self.isTraining = False
@@ -193,118 +302,6 @@ class BackJackGame:
         print(f"Total Wins (Win + Dealer Bust): {totalWins} rounds - {totalWinsPercent}%")
         print(f"Total Loses (Lose + Player Bust): {totalLoses} rounds - {totalLosesPercent}%")
         print(f"Draw: {drawNum} rounds - {drawPercent}%")
-
-class Player:
-    def __init__(self):
-        self.cards = []
-        pass
-
-    def drawCard(self):
-        self.cards.append(DeckHelpers.getCard())
-    
-    def drawBlackCard(self):
-        self.cards.append(DeckHelpers.getBlackCard())
-
-    def emptyCards(self):
-        self.cards = []
-    
-    def is21AboveOrBelow1(self):
-        playerSum = DeckHelpers.getSumOfCards(self.cards)
-        return (playerSum >= 21 or playerSum < 1)
-    
-    def promptAction(self):
-        playerInput = input("What would you like to do? Hit or Stick? (h/s)\n")
-        while playerInput != "h" and playerInput != "s":
-            playerInput = input("What would you like to do? Hit or Stick? (h/s)\n")
-        print("You have inputted: ", playerInput)
-        return playerInput
-    
-    def displayCards(self):
-        print("Your cards: ", self.cards)
-        print("Sum of cards: ", DeckHelpers.getSumOfCards(self.cards))
-
-
-class Dealer:
-    def __init__(self):
-        self.cards = []
-        pass
-
-    def drawCard(self):
-        self.cards.append(DeckHelpers.getCard())
-    
-    def drawBlackCard(self):
-        self.cards.append(DeckHelpers.getBlackCard())
-
-    def emptyCards(self):
-        self.cards = []
-    
-    def isAbove17orBust(self):
-        dealerSum = DeckHelpers.getSumOfCards(self.cards)
-        return dealerSum >= 17        
-
-
-class Qlearning(Player):
-    def __init__(self, epsilon=0.1, alpha=0.3, gamma=0.9):
-        super().__init__()
-        self.epsilon=epsilon
-        self.alpha=alpha
-        self.gamma=gamma
-        self.Q = {} #Q table
-        self.last_state=None
-        self.q_last=0.0
-        self.state_action_last=None
-
-    def resetLastStateVariables(self):
-        self.last_state = None
-        self.q_last = 0.0
-        self.state_action_last = None
-
-    def epslion_greedy(self, possible_moves): #esplion greedy algorithm
-        self.last_state = DeckHelpers.getSumOfCards(self.cards)
-        if(random.random() < self.epsilon):
-            move = random.choice(possible_moves)
-            self.state_action_last = (self.last_state, move)
-            self.q_last = self.getQ(self.last_state, move)
-            return move
-        else: #greedy strategy
-            Q_list=[]
-            for action in possible_moves:
-                Q_list.append(self.getQ(self.last_state, action))
-            maxQ=max(Q_list)
-
-            if Q_list.count(maxQ) > 1:
-                # more than 1 best option; choose among them randomly
-                best_options = [i for i in range(len(possible_moves)) if Q_list[i] == maxQ]
-                i = random.choice(best_options)
-            else:
-                i = Q_list.index(maxQ)
-            self.state_action_last = (self.last_state, possible_moves[i])
-            self.q_last = self.getQ(self.last_state, possible_moves[i])
-            return possible_moves[i]
-
-    def getQ(self, state, action): #get Q states
-        if(self.Q.get((state, action))) is None:
-            self.Q[(state, action)] = 0
-        return self.Q.get((state, action))
-
-    def updateQ(self, reward, possible_moves): # update Q states using Qleanning
-        q_list=[]
-        for moves in possible_moves:
-            q_list.append(self.getQ(DeckHelpers.getSumOfCards(self.cards), moves))
-        if q_list:
-            max_q_next = max(q_list)
-        else:
-            max_q_next = 0.0
-        self.Q[self.state_action_last] = self.q_last + self.alpha * ((reward + self.gamma * max_q_next) - self.q_last)
-
-    def saveQtable(self,file_name):  #save table
-        with open(file_name, 'wb') as handle:
-            pickle.dump(self.Q, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-    def loadQtable(self,file_name): # load table
-        with open(file_name, 'rb') as handle:
-            self.Q = pickle.load(handle)
-
 
 ## Training
 trainIterations = 10000
